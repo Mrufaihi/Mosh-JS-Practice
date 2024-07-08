c2f895067f28e7a5cf857e33d68a954de3ad72c4
Mrufaihi <113942663+Mrufaihi@users.noreply.github.com>
2024-07-08 19:08:03 +0300

    Exercise #1 Swapping Vars

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("hi");
+// excercise #1 Swapping vars
+// swap vars values at log
+
+// let a = 'red';
+// let b = 'blue';
+
+// MY ANSWER //
+// ternary
+// a = 'red' ? 'blue' : 'red';
+// b = 'blue' ? 'red' : 'blue';
+
+// console.log(a);
+// console.log(b);
+
+// MOSH ANSWER //
+let a = 'red';
+let b = 'blue';
+
+let c = a; //c = 'red'
+a = b; // a= 'blue'
+b = c; //b = 'red'
+
+console.log(a);
+console.log(b);
