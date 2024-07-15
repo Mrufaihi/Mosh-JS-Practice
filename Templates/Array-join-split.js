a3c280b9c63eb3024e91e0baf0c75f4b888ac522
Unranked <malrufaihi@gmail.com>
2024-07-15 17:51:44 +0300

    Array-join-split.js

---
+++ Templates/Array-join-split.js
@@ -0,0 +1,9 @@
+//join
+const array = ['ahmed', 'moe', 1, 2];
+console.log(array.join(',')) //join thme into 1 string| ',' between
+
+//split
+const message = 'this is my first message';
+const messageSplit= (message.split(' ')); // split only for String | use space to split them.
+
+console.log(messageSplit.join('-')) //| join only works for Array(messageSplit)
